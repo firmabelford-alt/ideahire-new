e217522c178efc46a01e106503a2a093c4b208ca
firmabelford-alt <firmabelford@gmail.com>
2026-08-14 16:23:47 +0200

    Connect application router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Router from "./router";
+import "./App.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Router />
+  </React.StrictMode>
 );
