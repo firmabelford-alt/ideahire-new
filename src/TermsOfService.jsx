1be03ca5e1bb815f7fbc2afb28bc21813c5c95a6
firmabelford-alt <firmabelford@gmail.com>
2026-09-06 22:51:18 +0200

    Update TermsOfService.jsx

--- src/TermsOfService.jsx
+++ src/TermsOfService.jsx
@@ -1,1 +1,8 @@
 
+import React from "react";
+import LegalDocument from "./LegalDocument";
+import termsHtml from "./legal/terms-pl.html?raw";
+
+export default function TermsOfService() {
+  return <LegalDocument kind="terms" html={termsHtml} />;
+}
